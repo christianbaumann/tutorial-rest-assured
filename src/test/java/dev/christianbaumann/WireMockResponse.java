e41a9a5264ead006589b465c16ec5b70011a7903
christian.baumann@maibornwolff.de <christian.baumann@maibornwolff.de>
2023-03-03 09:40:31 +0100

    Removed unused code

--- src/test/java/dev/christianbaumann/WireMockResponse.java
+++ src/test/java/dev/christianbaumann/WireMockResponse.java
@@ -15,16 +15,9 @@
  */
 package dev.christianbaumann;
 
-import com.google.common.collect.ImmutableListMultimap;
-import com.google.common.collect.Multimap;
 import org.apache.hc.core5.http.ClassicHttpResponse;
-import org.apache.hc.core5.http.Header;
-
-import java.nio.charset.Charset;
 
 import static com.github.tomakehurst.wiremock.common.HttpClientUtils.getEntityAsByteArrayAndCloseStream;
-import static com.google.common.base.Charsets.UTF_8;
-import static com.google.common.collect.Iterables.getFirst;
 
 public class WireMockResponse {
 
@@ -38,34 +31,5 @@ public class WireMockResponse {
 
     public int statusCode() {
         return httpResponse.getCode();
-    }
-
-    public String content() {
-        if (content == null) {
-            return null;
-        }
-        return new String(content, Charset.forName(UTF_8.name()));
-    }
-
-    public byte[] binaryContent() {
-        return content;
-    }
-
-    public String firstHeader(String key) {
-        return getFirst(headers().get(key), null);
-    }
-
-    public Multimap<String, String> headers() {
-        ImmutableListMultimap.Builder<String, String> builder = ImmutableListMultimap.builder();
-
-        for (Header header : httpResponse.getHeaders()) {
-            builder.put(header.getName(), header.getValue());
-        }
-
-        return builder.build();
-    }
-
-    public String statusMessage() {
-        return httpResponse.getReasonPhrase();
     }
 }
