4cf77a0a310717c0e66c319a6ae5f15bc99b060c
christian.baumann@maibornwolff.de <christian.baumann@maibornwolff.de>
2023-03-03 09:32:21 +0100

    Fixed formatting

--- src/test/java/dev/christianbaumann/WireMockResponse.java
+++ src/test/java/dev/christianbaumann/WireMockResponse.java
@@ -28,44 +28,44 @@ import static com.google.common.collect.Iterables.getFirst;
 
 public class WireMockResponse {
 
-  private final ClassicHttpResponse httpResponse;
-  private final byte[] content;
+    private final ClassicHttpResponse httpResponse;
+    private final byte[] content;
 
-  public WireMockResponse(ClassicHttpResponse httpResponse) {
-    this.httpResponse = httpResponse;
-    content = getEntityAsByteArrayAndCloseStream(httpResponse);
-  }
-
-  public int statusCode() {
-    return httpResponse.getCode();
-  }
-
-  public String content() {
-    if (content == null) {
-      return null;
-    }
-    return new String(content, Charset.forName(UTF_8.name()));
-  }
-
-  public byte[] binaryContent() {
-    return content;
-  }
-
-  public String firstHeader(String key) {
-    return getFirst(headers().get(key), null);
-  }
-
-  public Multimap<String, String> headers() {
-    ImmutableListMultimap.Builder<String, String> builder = ImmutableListMultimap.builder();
-
-    for (Header header : httpResponse.getHeaders()) {
-      builder.put(header.getName(), header.getValue());
+    public WireMockResponse(ClassicHttpResponse httpResponse) {
+        this.httpResponse = httpResponse;
+        content = getEntityAsByteArrayAndCloseStream(httpResponse);
     }
 
-    return builder.build();
-  }
+    public int statusCode() {
+        return httpResponse.getCode();
+    }
 
-  public String statusMessage() {
-    return httpResponse.getReasonPhrase();
-  }
+    public String content() {
+        if (content == null) {
+            return null;
+        }
+        return new String(content, Charset.forName(UTF_8.name()));
+    }
+
+    public byte[] binaryContent() {
+        return content;
+    }
+
+    public String firstHeader(String key) {
+        return getFirst(headers().get(key), null);
+    }
+
+    public Multimap<String, String> headers() {
+        ImmutableListMultimap.Builder<String, String> builder = ImmutableListMultimap.builder();
+
+        for (Header header : httpResponse.getHeaders()) {
+            builder.put(header.getName(), header.getValue());
+        }
+
+        return builder.build();
+    }
+
+    public String statusMessage() {
+        return httpResponse.getReasonPhrase();
+    }
 }
